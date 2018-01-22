2e1eed5c5061674c07b6407c955596f657be4de5
frankamente <fran29400@gmail.com>
2018-01-22 21:23:35 +0100

    Create simple serialize and deserialize using file

--- src/main/java/JsonSerializer.java
+++ src/main/java/JsonSerializer.java
@@ -5,36 +5,45 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class JsonSerializer {
+
+    private ObjectMapper objectMapper;
+
+    public JsonSerializer() {
+        this.objectMapper = new ObjectMapper();
+    }
+
     public static void main(String[] args) {
 
         JsonSerializer jsonSerializer = new JsonSerializer();
         Dog dogToSerialize = new Dog(15, new BasicInformation("perrete", 2));
 
         final String filePath = "dog.json";
-        jsonSerializer.SerializeDog(dogToSerialize, filePath);
+        jsonSerializer.serializeDogUsingFile(dogToSerialize, filePath);
 
-        Dog dogDeserialized = jsonSerializer.DeserializeDog(filePath);
+        Dog dogDeserialized = jsonSerializer.deserializeDogUsingFile(filePath);
 
-        System.out.println(dogToSerialize.equals(dogDeserialized)?"Los perros son iguales":"Ha habido un error, son distintos");
+        System.out.println(dogToSerialize.equals(dogDeserialized) ? "Los perros son iguales usando Archivo" : "Ha habido un error, son distintos usando archivos");
+
+        String dogInString = jsonSerializer.serializeDog(dogToSerialize);
+
+        dogDeserialized = jsonSerializer.deserializeDogUsingString(dogInString);
+
+        System.out.println(dogToSerialize.equals(dogDeserialized) ? "Los perros son iguales usando String" : "Ha habido un error, son distintos usando string");
     }
 
-    private Dog DeserializeDog(String json) {
+    private Dog deserializeDogUsingFile(String filePath) {
         try {
-            final ObjectMapper objectMapper = new ObjectMapper();
-            return objectMapper.readValue(new File(json), Dog.class);
+            return objectMapper.readValue(new File(filePath), Dog.class);
         } catch (IOException e) {
-            System.out.println("Error leyendo del json");
+            System.out.println("Error abriendo el fichero");
             System.out.println(e.getMessage());
         }
         return Dog.PerroInvalido;
     }
 
-    private void SerializeDog(Dog dog, String filePath) {
-        ObjectMapper objectMapper = new ObjectMapper();
+    private void serializeDogUsingFile(Dog dog, String filePath) {
 
         try {
             objectMapper.writeValue(new File(filePath), dog);
@@ -48,5 +57,24 @@ public class JsonSerializer {
             e.printStackTrace();
             System.out.println("Error creando archivo");
         }
+    }
+
+    private String serializeDog(Dog dog) {
+
+        try {
+            return objectMapper.writeValueAsString(dog);
+        } catch (JsonProcessingException e) {
+            System.out.println(e.getMessage());
+        }
+        return "";
+    }
+
+    private Dog deserializeDogUsingString(String dogInString) {
+        try {
+            return objectMapper.readValue(dogInString, Dog.class);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return Dog.PerroInvalido;
     }
 }
